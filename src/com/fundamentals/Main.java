3132f557f890b77c36f474569169f3f35d72d188
Mekebeb Geremew <hiwmek@gmail.com>
2020-12-04 13:38:48 -0600

    Lesson 5 Primitive data type and more

--- src/com/fundamentals/Main.java
+++ src/com/fundamentals/Main.java
@@ -1,5 +1,7 @@
 package com.fundamentals;
 
+import com.fundamentals.lessons.Lesson5;
+
 /*
 * This Main class is the entry point to the application
 * */
@@ -7,10 +9,21 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("Hello Java");
+        //System.out.println("Hello Java");
 
-        House myHouse = new House();
+        //House myHouse = new House();
+lesson5Examples();
+    } // end main methods
 
+    public static void lesson5Examples(){
+        Lesson5 myLesson5 = new Lesson5();
+        myLesson5.showLocalExample();
+        myLesson5.showLocalWithParameter(10,15);
+        int value = myLesson5.showLocalWithReturn(25,19);
+        System.out.println(value);
+        System.out.println(myLesson5.showLocalWithReturn(5,8));
+        myLesson5.showInstanceVariable();
+        myLesson5.showConversions();
+    } // end lesson5Example methods
 
-    }
-}
+} // end class
